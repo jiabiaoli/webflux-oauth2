e91a5dbdbfe95a50f3e3c2166b2d992718abe275
jiabiaoli <jiabiaoli@126.com>
2020-05-14 12:44:50 +0800

    init

---
+++ src/main/java/com/square/itsm/oauth2/Oauth2Application.java
@@ -0,0 +1,16 @@
+package com.square.itsm.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiabiaoli
+ */
+@SpringBootApplication
+public class Oauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Application.class, args);
+	}
+
+}
